b9ad494767c2836151e6be6776c91d71418f5afd
Shady-Max <kolodin2005+github@gmail.com>
2024-04-20 12:46:08 +0500

    added location

--- handlers.py
+++ handlers.py
@@ -5,5 +5,18 @@ from aiogram.filters import Command
 router = Router()
 
 @router.message(Command("start"))
-async def start():
-    pass
+async def start(message: Message):
+    button = types.KeyboardButton(text = "Отправить свою геолокацию", request_location=True)
+    keyboard = types.ReplyKeyboardMarkup(keyboard= [[button]], one_time_keyboard=True)
+    await message.answer("Привет! Я помогу тебе построить маршрут по городу для начала работы предоставь доступ к геолокации.", reply_markup = keyboard)
+
+@router.message()
+async def handle_text_message(message: types.Message):
+    if message.location is not None:
+        await handle_location(message)
+
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    reply = "latitude:  {}\nlongitude: {}".format(lat, lon)
+    await message.answer(reply)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 import aiogram
+import logging
+import sqlite3
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
@@ -10,9 +12,11 @@ import config
 
 
 async def main():
-    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
+    
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
