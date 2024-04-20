a8dd7cfd6bd4fc48c3342bfa0308e3185b2213f8
Shady-Max <kolodin2005+github@gmail.com>
2024-04-20 13:22:46 +0500

    location added

--- handlers.py
+++ handlers.py
@@ -1,14 +1,18 @@
+import sqlite3
 from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
 router = Router()
 
+def get_location_keyboard():
+    button = types.KeyboardButton(text = "Отправить свою геолокацию", request_location=True)
+    keyboard = types.ReplyKeyboardMarkup(keyboard= [[button]], one_time_keyboard=True, resize_keyboard=True)
+    return keyboard
+
 @router.message(Command("start"))
 async def start(message: Message):
-    button = types.KeyboardButton(text = "Отправить свою геолокацию", request_location=True)
-    keyboard = types.ReplyKeyboardMarkup(keyboard= [[button]], one_time_keyboard=True)
-    await message.answer("Привет! Я помогу тебе построить маршрут по городу для начала работы предоставь доступ к геолокации.", reply_markup = keyboard)
+    await message.answer("Привет! Я помогу тебе построить маршрут по городу для начала работы предоставь доступ к геолокации.", reply_markup = get_location_keyboard())
 
 @router.message()
 async def handle_text_message(message: types.Message):
@@ -18,6 +22,14 @@ async def handle_text_message(message: types.Message):
 async def handle_location(message: types.Message):
     lat = message.location.latitude
     lon = message.location.longitude
-    
-    reply = "latitude:  {}\nlongitude: {}".format(lat, lon)
+    with  sqlite3.connect('db.sqlite') as connection :
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM users WHERE telegram_id = {message.from_user.id}")
+        if cursor.fetchone() is None:
+            cursor.execute(f"INSERT INTO users VALUES ({message.from_user.id}, '{lat}', '{lon}')")
+        else:
+            cursor.execute(f"UPDATE users SET lat = {lat}, long = {lon} WHERE telegram_id = {message.from_user.id}")
+        connection.commit()
+        cursor.close()
+    reply = "Теперь вы можете..."
     await message.answer(reply)
--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from handlers import router
 
 import config
 
-
 async def main():
     with  sqlite3.connect('db.sqlite') as connection :
         cursor = connection.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS users (telegram_id INT PRIMARY KEY, lang REAL, long REAL)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (telegram_id INT PRIMARY KEY, lat REAL, long REAL)")
+        #cursor.execute("CREATE TABLE IF NOT EXISTS places ()")
+        connection.commit()
         cursor.close()
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=config.BOT_TOKEN)
