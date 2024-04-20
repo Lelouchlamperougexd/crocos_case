59614140ed708f6a1d44cf23ef02f41a7009dea1
Lelouchlamperougexd <ri4muse@gmail.com>
2024-04-20 17:59:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "7138822044:AAExURt0oXx5L02zT3nMmthyc4dv96n5cLo"
 GOOGLE_API = "AIzaSyC7jAtu0I7f4loSdVPbMxRXHk0ft-h9PI8"
-openai_api_key = 'sk-proj-3LLDsYMPAuEFDtoM8ho7T3BlbkFJ1CgVgi1n0E2IdUgPPiTu'
+openai_api_key = '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'
